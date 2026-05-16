9ba45ef5685695fb118f0aa6f752d85a1c18a6ca
nafisasraboni <148109529+nafisasraboni@users.noreply.github.com>
2026-05-16 21:38:56 +0600

    Time: 31 ms (88.98%) | Memory: 72.7 MB (48.02%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,30 +3,35 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  nums.sort((a, b) => a - b);
-        const count = new Map();
-        for (let num of nums) {
-            count.set(num, (count.get(num) || 0) + 1);
-        }
+    nums.sort((a,b)=>a-b);
+    const res=[];
 
-        const res = [];
-        for (let i = 0; i < nums.length; i++) {
-            count.set(nums[i], count.get(nums[i]) - 1);
-            if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>0)break;
+        if(i>0 && nums[i]===nums[i-1])continue;
 
-            for (let j = i + 1; j < nums.length; j++) {
-                count.set(nums[j], count.get(nums[j]) - 1);
-                if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+        let l=i+1;
+        r=nums.length-1;
 
-                const target = -(nums[i] + nums[j]);
-                if (count.get(target) > 0) {
-                    res.push([nums[i], nums[j], target]);
+        while(l<r){
+            const Sum=nums[i]+nums[l]+nums[r];
+            if(Sum>0){
+                r--;
+            }else if(Sum<0){
+                    l++;
+                }else{
+                    res.push([nums[i],nums[l],nums[r]]);
+                    l++;
+                    r--;
+                    while(l>0 && nums[l]===nums[l-1]){
+                        l++;
+                    }
                 }
             }
-
-            for (let j = i + 1; j < nums.length; j++) {
-                count.set(nums[j], count.get(nums[j]) + 1);
-            }
         }
-        return res;  
-};
+        return res;
+    }
+    
+   
+
+  
