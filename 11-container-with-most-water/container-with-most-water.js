3e85db7b251155923b65f4ca52a58851d40f7952
nafisasraboni <148109529+nafisasraboni@users.noreply.github.com>
2026-05-18 02:24:41 +0600

    Time: 3 ms (49.48%) | Memory: 63.3 MB (42.41%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -3,19 +3,21 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let l = 0;
-        let r = height.length - 1;
-        let res = 0;
+    let l=0;
+    let r=height.length-1;
+    let res=0;
 
-        while (l < r) {
-            const area = Math.min(height[l], height[r]) * (r - l);
-            res = Math.max(res, area);
-            if (height[l] <= height[r]) {
-                l++;
-            } else {
-                r--;
-            }
+    while(l<r){
+        let width=r-l;
+        const area= Math.min(height[l],height[r])*(width);
+        res=Math.max(res,area);
+
+        if(height[l]<=height[r]){
+            l++;
+        }else{
+            r--;
         }
-        return res;
-    
+    }
+
+return res;
 };
