931bd18714a92560eca90277d25c4c3d872d4e5e
nafisasraboni <148109529+nafisasraboni@users.noreply.github.com>
2026-07-04 22:46:28 +0600

    Time: 3 ms (90.76%) | Memory: 56.2 MB (89.07%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let stack=[];
+    for (const c of tokens) {
+            if (c === '+') {
+                stack.push(stack.pop() + stack.pop());
+            } else if (c === '-') {
+                const a = stack.pop();
+                const b = stack.pop();
+                stack.push(b - a);
+            } else if (c === '*') {
+                stack.push(stack.pop() * stack.pop());
+            } else if (c === '/') {
+                const a = stack.pop();
+                const b = stack.pop();
+                stack.push(Math.trunc(b / a));
+            } else {
+                stack.push(parseInt(c));
+            }
+        }
+        return stack.pop();
+};
