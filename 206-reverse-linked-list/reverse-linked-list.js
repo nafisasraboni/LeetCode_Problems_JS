34966a76128cd7ec399650f8d7521eb0b84dadb2
nafisasraboni <148109529+nafisasraboni@users.noreply.github.com>
2026-07-21 02:44:53 +0600

    Time: 1 ms (8.90%) | Memory: 57.6 MB (62.17%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -10,7 +10,7 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    let prev = null;
+    let prev=null;
     let current = head;
 
     while(current!==null){
@@ -19,6 +19,5 @@ var reverseList = function(head) {
         prev=current;
         current=next;
     }
-    return prev;
-    
+ return prev;
 };
