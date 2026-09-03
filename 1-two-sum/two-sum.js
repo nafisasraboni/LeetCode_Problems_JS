8bc37a96400876ece32c8c8df5c24e4b22471d61
Nafisa Tabassum Sraboni <nafisasraboni2207@gmail.com>
2026-09-03 23:31:00 +0600

    Time: 30 ms (39.99%) | Memory: 55.9 MB (21.49%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,16 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const prevMap = new Map();
-
-        for (let i = 0; i < nums.length; i++) {
-            const diff = target - nums[i];
-            if (prevMap.has(diff)) {
-                return [prevMap.get(diff), i];
-            }
-
-            prevMap.set(nums[i], i);
-        }
-
-        return [];
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]===target){
+            return [i,j];
+        }        
+    }
+}   
 };
