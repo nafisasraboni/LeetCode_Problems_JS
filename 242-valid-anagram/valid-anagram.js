7dc5fe70dd7cce1010d7259cb36a0200c1207067
Nafisa Tabassum Sraboni <nafisasraboni2207@gmail.com>
2026-09-06 18:52:44 +0600

    Time: 3 ms (99.33%) | Memory: 53.7 MB (99.17%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+            return false;
+        }
+
+        const count = new Array(26).fill(0);
+        for (let i = 0; i < s.length; i++) {
+            count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+            count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+        }
+        return count.every((val) => val === 0);
+};
