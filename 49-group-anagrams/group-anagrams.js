e011b1d90b0cfce01e8c66db6048002f2cfdeadf
Nafisa Tabassum Sraboni <nafisasraboni2207@gmail.com>
2026-09-10 00:10:27 +0600

    Time: 44 ms (17.98%) | Memory: 69.9 MB (9.89%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -4,19 +4,16 @@
  */
 var groupAnagrams = function(strs) {
     const res = {};
-    for (var s of strs){
-        const count = new Array(26).fill(0); 
-        for (var c of s){
-            count[c.charCodeAt(0)-'a'.charCodeAt(0)]+=1;
-        }
+        for (let s of strs) {
+            const count = new Array(26).fill(0);
+            for (let c of s) {
+                count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+            }
             const key = count.join(',');
-            if(!res[key]){
-                res[key]=[];
+            if (!res[key]) {
+                res[key] = [];
             }
             res[key].push(s);
-       }
-       return Object.values(res);
-    };
-    
-
-
+        }
+        return Object.values(res);
+};
