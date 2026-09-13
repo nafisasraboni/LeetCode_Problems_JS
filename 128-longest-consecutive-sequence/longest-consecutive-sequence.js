a006bb106574bec63887b2f886f75d9db751e724
Nafisa Tabassum Sraboni <nafisasraboni2207@gmail.com>
2026-09-13 18:15:03 +0600

    Time: 55 ms (14.38%) | Memory: 85 MB (9.43%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -4,18 +4,18 @@
  */
 var longestConsecutive = function(nums) {
     const mp = new Map();
-    let res=0;
+        let res = 0;
 
-    for(let num of nums){
-        if(!mp.has(num)){
-            mp.set(num,
-            (mp.get(num-1)||0) + (mp.get(num+1)||0) +1);
-
-            mp.set(num-(mp.get(num-1)||0),mp.get(num));
-            mp.set(num+(mp.get(num+1)||0),mp.get(num));
-            res = Math.max(res,mp.get(num));
+        for (let num of nums) {
+            if (!mp.has(num)) {
+                mp.set(
+                    num,
+                    (mp.get(num - 1) || 0) + (mp.get(num + 1) || 0) + 1,
+                );
+                mp.set(num - (mp.get(num - 1) || 0), mp.get(num));
+                mp.set(num + (mp.get(num + 1) || 0), mp.get(num));
+                res = Math.max(res, mp.get(num));
+            }
         }
-    }
-    return res;
-
-}; 
+        return res;
+};
