0e79933ef47acead550fc6409c5dbdebc8aaaa25
Nafisa Tabassum Sraboni <nafisasraboni2207@gmail.com>
2026-09-26 00:38:26 +0600

    Time: 4 ms (16.32%) | Memory: 62.9 MB (62.63%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -3,21 +3,18 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let l=0;
-    let r=height.length-1;
-    let res=0;
+    let l = 0;
+        let r = height.length - 1;
+        let res = 0;
 
-    while(l<r){
-        let width=r-l;
-        const area= Math.min(height[l],height[r])*(width);
-        res=Math.max(res,area);
-
-        if(height[l]<=height[r]){
-            l++;
-        }else{
-            r--;
+        while (l < r) {
+            const area = Math.min(height[l], height[r]) * (r - l);
+            res = Math.max(res, area);
+            if (height[l] <= height[r]) {
+                l++;
+            } else {
+                r--;
+            }
         }
-    }
-
-return res;
+        return res;
 };
